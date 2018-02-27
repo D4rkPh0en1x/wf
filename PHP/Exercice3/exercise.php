e25c21e4cb048a076505ef71bd7e2d1884f8e867
D4rkPh0en1x <frank@datateam.lu>
2018-02-27 15:57:57 +0100

    update

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,17 @@
 <?php
 
-$input;
-$winner;
+$winsA = 0;
+$winsB = 0;
+foreach($input as $turn) {
+    list($carda, $cardb) = $turn;
+    if ($carda > $cardb) {
+        $winsA++;
+    } else if ($cardb > $carda) {
+        $winsB++;
+    }
+}
+if ($winsA > $winsB) {
+    $winner = 'A';
+} else {
+    $winner = 'B';
+}
