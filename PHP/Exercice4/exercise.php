d51913c42e6341ea9bacda5e14388b119184a416
D4rkPh0en1x <frank@datateam.lu>
2018-02-28 10:26:07 +0100

    update

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,16 +1,16 @@
 <?php
 
 function getAllMondaysOfMonth($year, $month){
-        $mondays = [];
-        $date = DateTime::createFromFormat('Yn', $year.$month);
-        $date = new DateTime('first day of '.$date->format('F Y'));
-        $interval = DateInterval::createFromDateString('next monday');
-        if ($date->format('N') != 1) {
-            $date->add($interval);
+        $mondays = []; //Mondays array
+        $date = DateTime::createFromFormat('Yn', $year.$month); //change date format (Y=year n=number of the month without leading 0)
+        $date = new DateTime('first day of '.$date->format('F Y')); //change date format to first day of january 2018 (example)
+        $interval = DateInterval::createFromDateString('next monday'); //interval will be always the next monday - createFromDateString=Sets up a DateInterval from the relative parts of the string
+        if ($date->format('N') != 1) { //if date is not 1 -- N=day of the week without leading 0
+            $date->add($interval); //add predefined interval
         }
-        while($date->format('m') == $month) {
-            $mondays[] = $date->format('l j, M Y');
-            $date->add($interval);
+        while($date->format('m') == $month) { //while date still in the given month add mondays to the array
+            $mondays[] = $date->format('l j, M Y'); //this date format was asked by the todo file
+            $date->add($interval); //add predefined interval
         }
-    return $mondays;
+    return $mondays; //return mondays array
 }
